ee36522cbab928b388dd166490c3d162f9f236f1
raidgar98 <raidgar98@onet.pl>
2019-06-10 08:34:40 +0200

    layout improvement mechanism

--- cutealarm.cpp
+++ cutealarm.cpp
@@ -1,3 +1,5 @@
+#include <QFileDialog>
+#include <QMessageBox>
 #include "cutealarm.h"
 #include "ui_cutealarm.h"
 
@@ -6,9 +8,112 @@ CuteAlarm::CuteAlarm(QWidget *parent) :
 	ui(new Ui::CuteAlarm)
 {
 	ui->setupUi(this);
+	switcher(1);
 }
 
 CuteAlarm::~CuteAlarm()
 {
 	delete ui;
+}
+
+//Alarm
+void CuteAlarm::on_radioButton_toggled(bool checked)
+{
+	switcher(1);
+}
+
+//Minutnik
+void CuteAlarm::on_radioButton_2_toggled(bool checked)
+{
+	switcher(2);
+}
+
+//Stoper
+void CuteAlarm::on_radioButton_3_toggled(bool checked)
+{
+	switcher(3);
+}
+
+//Start
+void CuteAlarm::on_pushButton_clicked()
+{
+	ui->pushButton->setEnabled(false);
+	ui->pushButton_2->setEnabled(true);
+	ui->pushButton_5->setEnabled(ui->radioButton_3->isChecked());
+
+	//Finish layout
+}
+
+//Stop
+void CuteAlarm::on_pushButton_2_clicked()
+{
+	ui->pushButton_2->setEnabled(false);
+	ui->pushButton->setEnabled(true);
+	ui->pushButton_5->setEnabled(ui->radioButton_3->isChecked());
+
+}
+
+//Drzemka
+void CuteAlarm::on_pushButton_3_clicked()
+{
+
+}
+
+//Reset
+void CuteAlarm::on_pushButton_4_clicked()
+{
+/*	ui->pushButton->setEnabled(true);
+	ui->pushButton_2->setEnabled(false);
+	ui->pushButton_5->setEnabled(!ui->radioButton_3->isEnabled());*/
+	on_pushButton_2_clicked();
+}
+
+//Zapisz
+void CuteAlarm::on_pushButton_5_clicked()
+{
+
+}
+
+void CuteAlarm::switcher(const quint8 opt) noexcept
+{
+	ui->FirstEdit->setEnabled(false);
+	ui->SecondEdit->setEnabled(false);
+	ui->pushButton_2->setEnabled(false);
+	ui->pushButton_3->setEnabled(false);
+	ui->pushButton_4->setEnabled(false);
+	ui->pushButton_5->setEnabled(false);
+	ui->FirstEdit->setTime(QTime(0,0,0));
+	ui->SecondEdit->setTime(QTime(0,0,0));
+	ui->FirstRead->setTime(QTime(0,0,0));
+	ui->SecondRead->setTime(QTime(0,0,0));
+
+	switch(opt)
+	{
+		case 1:
+		{
+			ui->label->setText("Alarm o:");
+			ui->FirstEdit->setEnabled(true);
+			ui->SecondEdit->setEnabled(true);
+			ui->pushButton_2->setEnabled(true);
+			ui->pushButton_3->setEnabled(true);
+			ui->pushButton_4->setEnabled(true);
+
+			break;
+		}
+
+		case 2:
+		{
+			ui->label->setText("Alarm za:");
+			ui->FirstEdit->setEnabled(true);
+			ui->pushButton_4->setEnabled(true);
+			break;
+		}
+
+		case 3:
+		{
+			ui->pushButton_5->setEnabled(true);
+			ui->pushButton_4->setEnabled(true);
+			break;
+		}
+	}
 }
--- cutealarm.h
+++ cutealarm.h
@@ -1,21 +1,93 @@
 #ifndef CUTEALARM_H
 #define CUTEALARM_H
 
+#include <QThread>
 #include <QMainWindow>
+#include <QMediaPlayer>
 
 namespace Ui {
 class CuteAlarm;
 }
+
+class Clock : public QObject
+{
+	Q_OBJECT
+
+	quint64 _periodOfTime = 1000;
+	bool go = false;
+
+
+public:
+
+	explicit Clock(quint64 period, QObject * parent = nullptr)
+		:QObject(parent), _periodOfTime{period} {}
+
+	~Clock() {};
+
+	quint64 period() const noexcept { return _periodOfTime; }
+
+signals:
+
+	void period();
+
+public slots:
+
+	void Period()
+	{
+		go = true;
+		while (go)
+		{
+			QThread::currentThread()->msleep(_periodOfTime);
+			emit period();
+		}
+	}
+
+	void Stop()
+	{
+		go = false;
+	}
+
+};
 
 class CuteAlarm : public QMainWindow
 {
 	Q_OBJECT
 
 public:
+
 	explicit CuteAlarm(QWidget *parent = nullptr);
+
 	~CuteAlarm();
 
+	Clock * clock = nullptr;
+
+private slots:
+	void on_radioButton_toggled(bool checked);
+
+	void on_radioButton_2_toggled(bool checked);
+
+	void on_radioButton_3_toggled(bool checked);
+
+	void on_pushButton_clicked();
+
+	void on_pushButton_2_clicked();
+
+	void on_pushButton_3_clicked();
+
+	void on_pushButton_4_clicked();
+
+	void on_pushButton_5_clicked();
+
+signals:
+
+	void start();
+
+	void stop();
+
 private:
+
+	void switcher(const quint8 opt) noexcept;
+
 	Ui::CuteAlarm *ui;
 };
 
